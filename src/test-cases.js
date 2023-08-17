a3b94382e8480b7b7dc1379902d4322279d05e15
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-08-17 14:06:01 +0200

    Add code for passing tests for task no. 4

--- src/test-cases.js
+++ src/test-cases.js
@@ -46,3 +46,75 @@ export const calculator = {
     return a / b;
   },
 };
+
+// ----------------------------------------------------------------------//
+
+// 4. A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
+
+const latinLetters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+export function caesarCipher(str, shiftFactor) {
+  const lowerCaseStr = str.toLowerCase();
+  const letterList = latinLetters;
+  let encryptedArr = [];
+
+  // Push string indexes to array
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    if (/[a-zA-Z]/.test(lowerCaseStr[i])) {
+      encryptedArr.push(lowerCaseStr[i]);
+    } else {
+      encryptedArr.push(' ');
+    }
+  }
+
+  // Shift letters in array using shiftFactor and index-number from letter-array
+  for (let j = 0; j < encryptedArr.length; j++) {
+    const letterIndex = letterList.indexOf(encryptedArr[j]);
+    const shiftedLetterIndex = letterIndex + shiftFactor;
+    if (letterIndex === -1) {
+      encryptedArr[j] = ' ';
+    }
+    if (shiftedLetterIndex > letterList.length - 1) {
+      encryptedArr[j] =
+        letterList[shiftedLetterIndex - (letterList.length - 1) - 1];
+    } else if (letterIndex === -1) {
+      encryptedArr[j] = ' ';
+    } else {
+      encryptedArr[j] = letterList[shiftedLetterIndex];
+    }
+  }
+
+  // Convert shifted array indexes back into string
+  const encryptedString = encryptedArr.join('');
+
+  return encryptedString;
+}
+
+caesarCipher('defend.the-east:wall;of,the-castlezzz', 0);
